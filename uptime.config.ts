eddd845c163f8aee5a932c35794bdbfd508aa25a
Patrick <11915753+nacorid@users.noreply.github.com>
2024-10-11 11:27:39 +0200

    Update worker route

--- uptime.config.ts
+++ uptime.config.ts
@@ -45,7 +45,7 @@ const workerConfig = {
       //responseKeyword: 'success',
       // [OPTIONAL] if specified, the check will run in your specified region,
       // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-      //checkLocationWorkerRoute: 'https://xxx.example.com',
+      checkLocationWorkerRoute: 'https://de.naco.li',
     },
     {
       id: 'forgejo',
