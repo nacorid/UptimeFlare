5c887e66a9aedf049fe10c9b8958f10c26a3e2c7
Patrick <11915753+nacorid@users.noreply.github.com>
2024-10-23 16:06:16 +0200

    increase timeout

--- uptime.config.ts
+++ uptime.config.ts
@@ -33,7 +33,7 @@ const workerConfig = {
       // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
       expectedCodes: [200],
       // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
+      timeout: 30000,
       // [OPTIONAL] headers to be sent
       headers: {
         'User-Agent': 'Uptimeflare',
@@ -55,7 +55,7 @@ const workerConfig = {
       tooltip: 'Uptime of git.saneke.eu',
       statusPageLink: 'https://git.saneke.eu',
       expectedCodes: [200],
-      timeout: 10000,
+      timeout: 30000,
       headers: {
         'User-Agent': 'Uptimeflare',
       },
