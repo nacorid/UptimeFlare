dbc647930a76c17ce3e31420ac86b1ae374130a8
Patrick <11915753+nacorid@users.noreply.github.com>
2026-01-16 00:42:20 +0100

    fix: build error
    Added a maintenances array for scheduled maintenance configurations.

--- uptime.config.ts
+++ uptime.config.ts
@@ -212,5 +212,23 @@ const workerConfig: WorkerConfig = {
   },
 }
 
+const maintenances: MaintenanceConfig[] = []
+/*const maintenances: MaintenanceConfig[] = [
+  {
+    // [Optional] Monitor IDs to be affected by this maintenance
+    monitors: ['foo_monitor', 'bar_monitor'],
+    // [Optional] default to "Scheduled Maintenance" if not specified
+    title: 'Test Maintenance',
+    // Description of the maintenance, will be shown at status page
+    body: 'This is a test maintenance, server software upgrade',
+    // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
+    start: '2025-04-27T00:00:00+08:00',
+    // [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
+    // if not specified, the maintenance will be considered as on-going
+    end: '2025-04-30T00:00:00+08:00',
+    // [Optional] color of the maintenance alert at status page, default to "yellow"
+    color: 'blue',
+  },*/
+
 // Don't forget this, otherwise compilation fails.
-export { pageConfig, workerConfig }
+export { maintenances, pageConfig, workerConfig }
